23e17b5e1bd854bb182c61a3515f0bf3594c3b83
Cho-Hyun-Seung <toki0327@naver.com>
2023-11-16 02:24:47 +0900

    마이그레이션 파일  삭제

--- server/prisma/migrations/20231115171448_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `title` on table `Festival` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Festival" ALTER COLUMN "title" SET NOT NULL;
